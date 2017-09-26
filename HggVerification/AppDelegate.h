0d3e4b3a2e5a05cee251a832c55e5998b474bfa3
jinshuaier <470401910@qq.com>
2017-09-26 14:33:33 +0800

    Initial Commit

---
+++ HggVerification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HggVerification
+//
+//  Created by 胡高广 on 2017/9/26.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
